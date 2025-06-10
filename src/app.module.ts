d5ade02a258b5832100cc9732e3c5a0842678129
abhay <abhaygaming14122001@gmail.com>
2025-06-10 16:28:38 +0530

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [PostModule,
     ConfigModule.forRoot({
       envFilePath: '.env', // Path to your .env file
       isGlobal: true,      // Make config available globally
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI as string),
   ],
   controllers: [AppController],
   providers: [AppService],
